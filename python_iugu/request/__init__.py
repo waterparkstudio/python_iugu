43b2fb05bcc67bc80154b909e25726bfb7600cec
Lucas Paiva <lukspaiva95@gmail.com>
2021-09-09 15:43:39 -0300

    Update __init__.py

--- python_iugu/request/__init__.py
+++ python_iugu/request/__init__.py
@@ -2,3 +2,4 @@ from python_iugu.request import subscription_request
 from python_iugu.request import customer_request
 from python_iugu.request import invoice_request
 from python_iugu.request import utils_request
+from python_iugu.request import payment_method_request
